8c5db57ee31647b8082c6a24691b18f583875325
marcpalaci689 <marcpalaci689@gmail.com>
2022-12-29 12:22:52 -0500

    adding convai3d data

--- chatbot_data_formatting.py
+++ chatbot_data_formatting.py
@@ -8,6 +8,7 @@ import tensorflow as tf
 import re
 import numpy as np
 import pickle
+import json
 
 root_dir = Path(__file__).parent.absolute()
 
@@ -29,6 +30,7 @@ with open(os.path.join(data_dir,filename), 'r', encoding='utf-8') as file:
             questions.append(question)
             answers.append(answer)
 
+# prepare the chatbot data
 data_dir = os.path.join(root_dir, 'chatbot_data')
 print(data_dir)
 for filename in os.listdir(data_dir):
@@ -46,6 +48,31 @@ for filename in os.listdir(data_dir):
                 #        answer += ' {0}'.format(response)
                 questions.append(question)
                 answers.append(answer)
+
+# prepare the convai3d data
+data_dir = os.path.join(root_dir, 'convai3_data')
+with open(os.path.join(data_dir,'data_tolokers.json'), 'r', encoding='utf-8') as file:
+    data = json.load(file)
+for conv in data:
+    dialogue = conv['dialog']
+    if len(dialogue) >= 2:
+        while len(dialogue)>1:
+            # keep popping front until a convo is initiated
+            if dialogue[0]['sender_class'] == dialogue[1]['sender_class']:
+                dialogue.pop(0)
+                continue
+            else:
+                q = re.sub("[\(\[].*?[\)\]]", "", dialogue[0]['text'])
+                a = re.sub("[\(\[].*?[\)\]]", "", dialogue[1]['text'])
+                q = re.sub("[^A-Za-z0-9 -]", "", q).rstrip()
+                a = re.sub("[^A-Za-z0-9 -]", "", a).rstrip()
+                if len(a.split()) == 1 or len(q.split()) == 1:
+                    pass
+                else:
+                    questions.append(q)
+                    answers.append(a)
+                dialogue.pop(0)
+                dialogue.pop(0)
 
 answers_with_tags = []
 for i in range(len(answers)):
